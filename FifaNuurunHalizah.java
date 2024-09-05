c8f0052f0a070d53094692caca8f65b906d8c8a0
fifanuurunhalizah09 <eljeaje@gmail.com>
2024-09-05 12:41:14 +0700

    Commit Biodata Fifa

---
+++ FifaNuurunHalizah.java
@@ -0,0 +1,14 @@
+public class FifaNuurunHalizah {
+
+    public static void main(String[] args) {
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA--");
+        System.out.println("Nama    : Fifa Nuurun Halizah");
+        System.out.println("NIM     : 244107020019");
+        System.out.println("Kelas   : TI 1D");
+        System.out.println("No Absen: 09");
+        System.out.println("Prodi   : Teknik Informatika");
+        System.out.println("Alamat  : Gondanglegi Kab. Malang");
+        System.out.println("No HP   : 083151674894");
+        System.out.println("Hobi    : Bernyanyi");
+    }
+}
