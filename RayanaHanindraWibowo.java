1f5b64600f908845b10386218d68e91ec2a5f6f4
Rayhwcakep <rayhanindra290805@gmail.com>
2024-09-05 12:52:32 +0700

    Rayana hanindra wibowo

---
+++ RayanaHanindraWibowo.java
@@ -0,0 +1,15 @@
+public class RayanaHanindraWibowo{
+    public static void main(String[] args){
+        System.out.println("---------------------------------------------");
+        System.out.println("--Data Mahasiswa Jrusan Teklonogi Informasi--");
+        System.out.println("---------------------------------------------");
+        System.out.println("Nama         : Rayana Hanindra Wibowo");
+        System.out.println("NIM          : 244107020178");
+        System.out.println("kelas        : 1D ");
+        System.out.println("No Absen     : 26");
+        System.out.println("Prodi        : Teknik informatika");
+        System.out.println("Alamat       : Jln.Wijaya Kusuma No.78 ");
+        System.out.println("No HP        : 085717288330 ");
+        System.out.println("Hobi         : basket dan editing ");
+    }
+}
