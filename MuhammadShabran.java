d994ad28c2d7c8596c0e8b045cb20d03b22ca465
daebon07 <muhammadshabran25@gmail.com>
2024-09-05 12:07:29 +0700

    muhammad shabran

---
+++ MuhammadShabran.java
@@ -0,0 +1,15 @@
+public class MuhammadShabran{
+    public static void main(String[] args){
+        System.out.println("---------------------------------------------");
+        System.out.println("--Data Mahasiswa Jrusan Teklonogi Informasi--");
+        System.out.println("---------------------------------------------");
+        System.out.println("Nama         : Muhammad Shabran");
+        System.out.println("NIM          : 244107020112");
+        System.out.println("kelas        : 1D ");
+        System.out.println("No Absen     : 21");
+        System.out.println("Prodi        : Teknik informatika");
+        System.out.println("Alamat       : Kota Bima, NTB ");
+        System.out.println("No HP        : 085162632677 ");
+        System.out.println("Hobi         : Nonton Youtube ");
+    }
+}
